62ae6b592e3fd7e9f31419d4796b04a8736bab34
yashjariwala01 <122197521+yashjariwala01@users.noreply.github.com>
2023-02-15 12:10:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your code here
+function minCostRopes(a){
+	a.preventDefault();
+	
+	var inputElement = document.querySelector("input").value;
+	var arr = inputElement.split(',');
+    arr.sort(function(a,b){return a-b});
+
+    var cost =0;
+    while(arr.length>1){
+    var res = Number(arr[0]) + Number(arr[1]);
+    arr.splice(0,2);
+    arr.push(res);
+    //cost = cost + res;
+    cost += res;
+
+    arr.sort(function(a,b){return (a-b)});
+    }
+    document.getElementById("result").textContent=cost;
+}
